5f9ecc8823600649f0cd0d2db7ede716b17a2c94
Willson Wang <willson@Willsons-MBP.local>
2021-04-15 22:52:13 -0400

    styling

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const router = express.Router()
 
-const data = [{
-    id: 1,
-    name: "d1",
-    email: "e1",
-},
-{
-    id: 2,
-    name: "d2",
-    email: "e2",
-},
+const data = [
+    {
+        id: 1,
+        name: "d1",
+        email: "e1",
+    },
+    {
+        id: 2,
+        name: "d2",
+        email: "e2",
+    }
 ];
 
 router.get("/", (req, res) => {
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const router = express.Router()
 
-const users = [{
-    id: 1,
-    name: "u1",
-    email: "e1",
-},
-{
-    id: 2,
-    name: "u2",
-    email: "e2",
-},
+const users = [
+    {
+        id: 1,
+        name: "u1",
+        email: "e1",
+    },
+    {
+        id: 2,
+        name: "u2",
+        email: "e2",
+    }
 ];
 
 router.get("/", (req, res) => {
